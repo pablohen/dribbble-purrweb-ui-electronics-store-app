6371f102200d81691c54b9f329a513a01cbe7843
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-28 19:43:39 -0300

    fix system theme detection

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -3,14 +3,14 @@ import { SunIcon, MoonIcon } from '@heroicons/react/solid';
 import { useTheme } from 'next-themes';
 
 const ThemeButton = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setLoading(false);
   }, []);
 
   const changeTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
   };
 
   if (loading) return null;
@@ -20,7 +20,7 @@ const ThemeButton = () => {
       className="bg-white dark:bg-gray-900 rounded-lg p-2 transform transition-all duration-200 ease-in-out  hover:shadow"
       onClick={changeTheme}
     >
-      {theme === 'light' ? (
+      {resolvedTheme === 'light' ? (
         <SunIcon className="h-6" />
       ) : (
         <MoonIcon className="h-6" />
